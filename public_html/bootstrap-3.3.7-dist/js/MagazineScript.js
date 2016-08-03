6156d81713873f0fd016ff36416fa4c0a3c3f7fb
Ivan <air_dance@abv.bg>
2016-08-03 19:00:41 +0300

    alabala

---
+++ public_html/bootstrap-3.3.7-dist/js/MagazineScript.js
@@ -0,0 +1,10 @@
+function change(obj) {
+                if (obj.src == 'bootstrap-3.3.7-dist/fonts/png/glyphicons-194-ok-sign.png') //positive
+                    obj.src = 'bootstrap-3.3.7-dist/fonts/png/glyphicons-191-plus-sign.png';  //negative
+                else
+                    obj.src = 'bootstrap-3.3.7-dist/fonts/png/glyphicons-194-ok-sign.png'; //positive
+            };
+            
+
+            
+
